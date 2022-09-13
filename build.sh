9e787e71c0d27f4f35abb7b1cd7e960b6807b5c4
DonaldChung-HK <lpchungaa@gmail.com>
2022-09-13 09:12:12 +0100

    fixed xvfb

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-sudo apt install -y xvfb
-run_with_xvfb cat ./Multiline-log.txt
+xvfb-run -e /dev/stderr --server-args="-core -noreset -screen 0 640x480x24" --server-num=101 cat ./Multiline-log.txt
 terminate_xvfb_sessions
